0282f87cf4b3fad75f70c3abe967da6023a83630
unknown <jenya@web-dev1-PC.wisesec.local>
2015-05-07 13:16:41 +0300

    added a method to see only the public fields

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -1,16 +1,16 @@
 <?php
 	class Site extends CI_Controller {
 		function index() {
-			$t = new test();
+			$t = new testExtension2();
 			$t->getVars(); 
-			var_dump (get_class_vars('test'));
+//			var_dump(get_class_vars('test'));
+			$t->getVisibleVars();
 		}
 		
 		function index1() {
 			$this->load->model('data_model');
 			$data['rows'] = $this->data_model->getAll();
 			$this->load->view('home', $data);
-			
 		}
 		
 		// video 1 moethod
@@ -27,8 +27,36 @@
 		protected $b = 2;
 		private $c = 33;
 		
-		public function getVars(){
-			var_dump(get_class_vars(__CLASS__));	
+		public function getVars() {
+//			var_dump(get_class_vars(__CLASS__));
+			return get_class_vars(__CLASS__);	
 		} 
+	}
+	
+	class testExtension1 extends test {
+		public $t1 = 7;
+		protected $t2 = 8;
+		private $t3 = 9;
+		
+		public function getVars() {
+			$superVars = parent::getVars();
+			$visibleVars = get_class_vars(__CLASS__);
+//			var_dump(array_merge($superVars, $visibleVars));
+			return array_merge($superVars, $visibleVars);
+		}
+	}
+	class testExtension2 extends testExtension1 {
+		public $tt1 = 44;
+		protected $tt2 = 55;
+		private $tt3 = 66;
+		
+//		@Override
+		public function getVars() {
+			var_dump(array_merge(parent::getVars(), get_class_vars(__CLASS__)));
+		}
+		
+		public function getVisibleVars() {
+			var_dump(get_class_vars(__CLASS__));
+		}
 	}
 ?>
